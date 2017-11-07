36f28ac5a3b6c4b6d36b7180d9a475d353adc63d
DavidGuben <davidjg6@gmail.com>
2017-11-06 23:07:09 -0500

    add idea functionality

--- app.js
+++ app.js
@@ -46,9 +46,33 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
+// Idea Index Page
+app.get('/ideas', (req, res) => {
+  Idea.find({})
+    .sort({date: 'desc'})
+    .then(ideas => {
+      res.render('ideas/index', {
+        ideas: ideas
+      });
+    });
+});
+
+
 // Add Idea Form
 app.get('/ideas/add', (req, res) => {
   res.render('ideas/add');
+});
+
+// Edit Idea Form
+app.get('/ideas/edit/:id', (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  })
+  .then(idea => {
+    res.render('ideas/edit', {
+      idea: idea
+    });
+  });
 });
 
 // Process Form
